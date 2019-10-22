0d37d26b840a111fb877452490935d22f9b07bb9
Alexey Zhavoronkov <alex-13-05@mail.ru>
2019-10-22 20:14:41 +0300

    terser

--- rollup/babel.config.js
+++ rollup/babel.config.js
@@ -1,9 +1,11 @@
 import babel from 'rollup-plugin-babel';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: './src/index.js',
   plugins: [
-    babel({presets: ['@babel/preset-env']})
+    babel({presets: ['@babel/preset-env']}),
+    terser()
   ],
   output: {
     file: './dist/babel.js',
--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,5 +1,10 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
   input: './src/index.js',
+  plugins: [
+    terser()
+  ],
   output: {
     file: './dist/new.js',
     format: 'iife',
